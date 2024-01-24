97432b36b09559aa04882cf6694cd48134edac86
John Sener <john.sener@inspirationtech.co.uk>
2024-01-24 00:52:04 +0000

    Initial commit of all files to the repository

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CodeFirstLogin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstLogin.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CodeFirstLogin.Context;
+
+public class AccountController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AccountController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        // Validate login credentials against the database
+        var user = _context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+
+        if (user != null)
+        {
+            // Successful login, redirect to a dashboard or home page
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            // Failed login, return to the login page with an error message
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+    }
+}
